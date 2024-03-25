dbed1d4a297c8b6add6ec8f529d45f1885b4481c
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2024-03-24 21:59:09 -0600

    Tune PID constants

--- 2_2_wheelSpeedControl/2_2_wheelSpeedControl.ino
+++ 2_2_wheelSpeedControl/2_2_wheelSpeedControl.ino
@@ -2,8 +2,8 @@
 #include "src/DDR/DDR.h"
 
 // DDR
-PID_ks const left_pid_K = {0.0, 0.0, 0.0};
-PID_ks const right_pid_K = {0.0, 0.0, 0.0};
+PID_ks const left_pid_K = {0.1, 0.55, 0.0};
+PID_ks const right_pid_K = {0.09, 0.3, 0.0};
 PID_vars left_pid_vars = {left_pid_K, 0.0, 0, 0, 0};
 PID_vars right_pid_vars = {right_pid_K, 0.0, 0, 0, 0};
 uint8 const ins[] = {11, 10, 9, 6};
@@ -29,16 +29,12 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //float inSignal = analogRead(0);
-  ddr.forward(40);
-  uint8 leftRPM = ddr.leftWheel.u_velRPM;
+  ddr.forward(65);
   uint8 rightRPM = ddr.rightWheel.u_velRPM;
-  
-  //elapsedTimeLeft = ddr.getRPMLeft();
-  //elapsedTimeRight = ddr.getRPMRight();
-
-  Serial.println(leftRPM);
+  uint8 leftRPM = ddr.leftWheel.u_velRPM;
+  Serial.print(leftRPM);
+  Serial.print(",");
+  Serial.println(rightRPM);
 
   delay(100);
 }
