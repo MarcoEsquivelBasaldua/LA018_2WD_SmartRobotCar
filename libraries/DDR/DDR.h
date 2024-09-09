5e9f00c79ad633956edb2ad8210dd296813a48f6
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2024-09-09 00:21:10 -0600

    Update libraries

--- libraries/DDR/DDR.h
+++ libraries/DDR/DDR.h
@@ -15,28 +15,42 @@
 #include "Arduino.h"
 #include "../typeDefs/typeDefs.h"
 
+/******************* DEFINES *********************/
+#define  TOP_VEL_OFFSET       (1u)
+#define  BOTTOM_VEL_OFFSET    (9u)
+#define  STOP_RPM             (0u)                     /* RPM when stop */
+#define  MIN_SPPED_CONTROL    (50u)                    /* Minimum allowed wheel output (determined experimentally) */
+#define  MAX_SPPED_CONTROL    (255u - TOP_VEL_OFFSET)  /* Maximum allowed wheel output (full PWM) */
+#define  ONE_F                (1.0f)                   /* Constant 1 float*/
+
+#define  MAX(x,y)           ( ((x)>(y)) ? (x) : (y) ) /* Max function macro */
+#define  MIN(x,y)           ( ((x)<(y)) ? (x) : (y) ) /* Min function macro */
+
+/*************************************************/
+
 typedef struct Wheel{
-	uint8 IN1;
-	uint8 IN2;
-	uint8 SPEED_INTERRUPT;
+	uint8 u_in1;
+	uint8 u_in2;
 } Wheel; // End Wheel
 
 class DDR
 {
 	public:
 		DDR(Wheel const LEFTWHEEL, Wheel const RIGHTWHEEL);
-		void forward(uint8 vel);
-		void backward(uint8 vel);
-		void turnRight(uint8 vel);
-		void turnLeft(uint8 vel);
-		void turnRightFast(uint8 vel);
-		void turnLeftFast(uint8 vel);
+		void forward(uint8 const vel);
+		void backward(uint8 const vel);
+		void turnRight(uint8 const vel);
+		void turnLeft(uint8 const vel);
+		void turnRightFast(uint8 const vel);
+		void turnLeftFast(uint8 const vel);
 		void stop();
+		
 
+	private:
 		Wheel leftWheel;
 		Wheel rightWheel;
-	
-	//private:
 };
+
+uint8 getVelOffset(uint8 vel);
 
 #endif
