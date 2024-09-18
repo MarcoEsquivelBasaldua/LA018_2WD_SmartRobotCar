4e43d14c9fba8dd63ec912291bf5c641de7566a5
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2024-09-18 04:47:18 -0600

    Define indoor and out door speeds

--- 1_distanceKeeper/src/DDR/DDR.h
+++ 1_distanceKeeper/src/DDR/DDR.h
@@ -16,15 +16,17 @@
 #include "../typeDefs/typeDefs.h"
 
 /******************* DEFINES *********************/
-#define  TOP_VEL_OFFSET       (1u)
-#define  BOTTOM_VEL_OFFSET    (9u)
-#define  STOP_RPM             (0u)                     /* RPM when stop */
-#define  MIN_SPPED_CONTROL    (50u)                    /* Minimum allowed wheel output (determined experimentally) */
-#define  MAX_SPPED_CONTROL    (255u - TOP_VEL_OFFSET)  /* Maximum allowed wheel output (full PWM) */
-#define  ONE_F                (1.0f)                   /* Constant 1 float*/
+#define  TOP_VEL_OFFSET         (  1u)
+#define  BOTTOM_VEL_OFFSET      (  9u)
+#define  STOP_RPM               (  0u)                   /* RPM when stop                                            */
+#define  MIN_SPPED_CONTROL      ( 50u)                   /* Minimum allowed wheel output (determined experimentally) */
+#define  INDOOR_SPEED_CONTROL   ( 60u)                   /* Desired control for indoor usage                         */
+#define  OUTDOOR_SPEED_CONTROL  (100u)                   /* Desired control for outdoor usage                        */
+#define  MAX_SPPED_CONTROL      (255u - TOP_VEL_OFFSET)  /* Maximum allowed wheel output (full PWM)                  */
+#define  ONE_F                  (1.0f)                   /* Constant 1 float                                         */
 
-#define  MAX(x,y)           ( ((x)>(y)) ? (x) : (y) ) /* Max function macro */
-#define  MIN(x,y)           ( ((x)<(y)) ? (x) : (y) ) /* Min function macro */
+#define  MAX(x,y)           ( ((x)>(y)) ? (x) : (y) )  /* Max function macro */
+#define  MIN(x,y)           ( ((x)<(y)) ? (x) : (y) )  /* Min function macro */
 
 /*************************************************/
 
