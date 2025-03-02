c01be6037d34820128a133cc99b1b4c89653a837
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2025-03-01 22:35:08 -0600

    Add code to check app inputs

---
+++ 4_BT_controlled_ddr/BT_decoder/BT_decoder.ino
@@ -0,0 +1,38 @@
+#include "src/typeDefs/typeDefs.h"
+#include <LiquidCrystal_I2C.h>
+
+/**************************************************************************************
+*  Wiring
+*    _____________      _________________     ___________________
+*   |          VCC|<---|GND           5V |--->|VCC               |
+*   |          GND|<---|5V            GND|--->|GND               |
+*   |  HC-06    Tx|--->|Rx   ARDUINO   A4|--->|SDA  16x2 LCD_I2C |
+*   |           Rx|<---|Tx     UNO     A5|--->|SCL               |
+*   |_____________|    |_________________|    |__________________|
+*
+***************************************************************************************/
+
+LiquidCrystal_I2C lcd(0x27,16,2);  //
+
+void setup() {
+  // Bluetooth will communicate by Serial
+  Serial.begin(9600);
+
+  lcd.init();
+  lcd.backlight();
+  
+  lcd.print("Data received");
+}
+
+void loop() {
+  if (Serial.available())
+  {
+    // Read data received
+    uint8 c_command = Serial.read();
+
+    // Set cursor on second row
+    lcd.setCursor(0, 1);
+    // Show received data
+    lcd.print(c_command);
+  }
+}
