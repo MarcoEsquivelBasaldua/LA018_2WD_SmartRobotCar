bdb69c6ee7037459ed75c4cc3e394fcaae9b5f4f
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2025-03-08 01:07:27 -0600

    Set workspace up

---
+++ 5_obstacle_avoidance_car/obstacle_avoidance_car/obstacle_avoidance_car.ino
@@ -0,0 +1,13 @@
+#include "src/tpedefs/tpedefs.h"
+#include "src/DDR/DDR.h"
+#include "src/HCSR04/HCSR04.h"
+#include "src/myServo/myServo.h"
+#include "src/BT_encodedData/BT_encodedData.h"
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
